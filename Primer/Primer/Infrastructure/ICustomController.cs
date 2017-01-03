4a46f755406955f0e4b0caddc4acf81c957b453c
Ren <mjren@corp.tcc.inet>
2017-01-03 21:51:59 +0800

    Added code for primer, including Ajax, Knockoutjs async

---
+++ Primer/Primer/Infrastructure/ICustomController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Primer.Infrastructure
+{
+    public interface ICustomController
+    {
+        Task<long> GetPageSize(CancellationToken cToken);
+
+        Task PostUrl(string newUrl, CancellationToken cToken);
+    }
+}
