7006764178b2101e3ad22a7ea37ee42d8cc7223e
Ren <mjren@corp.tcc.inet>
2017-01-03 09:21:53 +0800

    Initialize

---
+++ PartyInvities/PartyInvities/Models/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvities.Models
+{
+    public class Repository
+    {
+        public static Dictionary<string, GuestResponse> responses;
+
+        public Repository()
+        {
+            responses = new Dictionary<string, GuestResponse>();
+            responses.Add("Bob", new GuestResponse
+            {
+                Name = "Bob",
+                Email = "bob@example.com",
+                WillAttend = true
+            });
+            responses.Add("Alice", new GuestResponse
+            {
+                Name = "Alice",
+                Email = "alice@example.com",
+                WillAttend = true
+            });
+            responses.Add("Paul", new GuestResponse
+            {
+                Name = "Paul",
+                Email = "paul@example.com",
+                WillAttend = true
+            });
+        }
+        public static void Add(GuestResponse newResponse)
+        {
+            string key = newResponse.Name.ToLowerInvariant();
+            if (responses.ContainsKey(key))
+            {
+                responses[key] = newResponse;
+            }
+            else
+            {
+                responses.Add(key, newResponse);
+            }
+        }
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return responses.Values; }
+        }
+    }
+}
